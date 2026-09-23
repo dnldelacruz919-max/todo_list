15202123b9898787a412de38a18d5b9b50e6f9cf
Daniela Joy S. Dela Cruz <dnldelacruz919@gmail.com>
2026-09-23 12:10:11 +0900

    Add todo heading functionality

---
+++ src/Components/TodoList.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import './TodoList.css';
+
+const TodoList = () => {
+
+    const [todos, setTodos] = useState([]);
+    const [headingInput, setHeadingInput] = useState('');
+    const [listInputs, setListInputs] = useState({});
+
+    const handleAddTodo = () => {
+      if (headingInput.trim() !== '') {
+          setTodos([...todos, { heading: headingInput, lists: [] }]);
+          setHeadingInput('');
+      }
+  };
+  
+     const handleDeleteTodo = (index) => {
+      const newTodos = [...todos];
+      newTodos.splice(index, 1);
+      setTodos(newTodos);
+  };
+  
+  return (
+    <>
+      <div className="todo-container">
+        <h1 className="title">My Todo List</h1>
+        <div className="input-container">
+        <input
+            type="text"
+            className="heading-input"
+            placeholder="Enter heading"
+            value={headingInput}
+            onChange={(e) => setHeadingInput(e.target.value)}
+        />
+
+        <button className="add-list-button" onClick={handleAddTodo}>
+            Add Heading
+        </button>
+        </div>
+      </div>
+      <div className="todo_main">
+    {todos.map((todo, index) => (
+        <div key={index} className="todo-card">
+            <h3>{todo.heading}</h3>
+            <button
+                className="delete-button-heading"
+                onClick={() => handleDeleteTodo(index)}
+            >
+                Delete Heading
+            </button>
+        </div>
+    ))}
+</div>
+    </>
+  );
+};
+
+export default TodoList;
